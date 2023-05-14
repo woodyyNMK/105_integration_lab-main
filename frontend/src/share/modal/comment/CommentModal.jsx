78be651baad341f21d5f375e10009eceb9e2a8f4
WooDyy <woodyy.nmk@gmail.com>
2023-05-14 18:20:18 +0700

    Integration of Comment Modal

--- frontend/src/share/modal/comment/CommentModal.jsx
+++ frontend/src/share/modal/comment/CommentModal.jsx
@@ -1,20 +1,85 @@
 import { Box, Button, Card, Modal, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useKeyDown } from '../../../hooks/useKeyDown';
 import CommentCard from './components/CommentCard';
+import Cookies from 'js-cookie';
+import { AxiosError } from 'axios';
+import Axios from '../../AxiosInstance';
+import GlobalContext from '../../../share/Context/GlobalContext';
 
 const CommentModal = ({ open = false, handleClose = () => {} }) => {
   const [textField, setTextField] = useState('');
   const [comments, setComments] = useState([]);
+  const [error, setError] = useState('');
+  const { setStatus } = useContext(GlobalContext);
 
   useKeyDown(() => {
     handleAddComment();
   }, ['Enter']);
 
-  const handleAddComment = () => {
+  useEffect(() => {
+    //get comments by user's token
+    //check if user is logged in
+    const userToken = Cookies.get('UserToken');
+    if (userToken !== undefined && userToken !== 'undefined') {
+      //call API to get comments
+      Axios.get('/comment', { headers: { Authorization: `Bearer ${userToken}`}}).then((res)=> {
+        //set comments to state comments
+        setComments(res.data.data);
+      })
+    }
+  },[comments]);
+
+  const validateForm = () => {
+    if(!textField){
+      setError('Input required')
+      return false;
+    } 
+    setError("");
+    setTextField("");
+    return true;
+  }
+
+  const handleAddComment = async () => {
     // TODO implement logic
-    setComments([...comments, { id: Math.random(), msg: textField }]);
+    if(!validateForm()) return;
+
+    try{
+      // setComments([...comments, { id: Math.random(), msg: textField }]);
+      const userToken = Cookies.get('UserToken');
+      const response = await Axios.post('/comment',{ text: textField } , { 
+        headers: { Authorization: `Bearer ${userToken}`},
+      });
+      if(response.data.success) {
+        setStatus({ severity: 'success', msg: 'Create comment successfully' });
+        setComments((prev) => [...prev, response.data.data]);
+        // setTextField('');
+        resetAndClose();
+      }
+    }catch (error){
+      // 4. if create note failed, check if error is from calling API or not
+      if( error instanceof AxiosError && error.response) {
+        setStatus({
+          severity: 'error',
+          msg: error.response.data.error
+        });
+      }else {
+        setStatus({
+          severity: 'error',
+          msg: error.message
+        });
+      }
+    }
   };
+
+  const resetAndClose = () => {
+    setTimeout(() => {
+      setTextField('');
+      setError('');
+    }, 500);
+    handleClose();
+  };
+
 
   return (
     <Modal open={open} onClose={handleClose}>
@@ -39,6 +104,8 @@ const CommentModal = ({ open = false, handleClose = () => {} }) => {
         >
           <TextField
             value={textField}
+            error={error !== ''}
+            helperText={error}
             onChange={(e) => setTextField(e.target.value)}
             fullWidth
             placeholder="Type your comment"
--- frontend/src/share/modal/comment/components/CommentCard.jsx
+++ frontend/src/share/modal/comment/components/CommentCard.jsx
@@ -1,23 +1,64 @@
 import { Button, Card, TextField, Typography } from '@mui/material';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useContext } from 'react';
+import Cookies from 'js-cookie';
+import Axios from '../../../AxiosInstance';
+import GlobalContext from '../../../Context/GlobalContext';
 
-const CommentCard = ({ comment = { id: -1, msg: '' } }) => {
+const CommentCard = ({ comment = { id: -1, text: '' }, comments,setComments = ()=>{} }) => {
+  const { user, setStatus } = useContext(GlobalContext);
   const [isConfirm, setIsConfirm] = useState(false);
   const [functionMode, setFunctionMode] = useState('update');
-  const [msg, setMsg] = useState(comment.msg);
+  const [msg, setMsg] = useState(comment.text);
+  const [error, setError] = useState('');
+  const [id, setId] = useState(comment.id);
 
   const submit = useCallback(() => {
-    if (functionMode === 'update') {
+    const userToken = Cookies.get('UserToken');
+    if(userToken !== undefined && userToken !== 'undefined') {
+      if (functionMode === 'update') {
       // TODO implement update logic
-      console.log('update');
-    } else if (functionMode === 'delete') {
-      // TODO implement delete logic
-      console.log('delete');
-    } else {
-      // TODO setStatus (snackbar) to error
-      console.log('error');
+        Axios.patch('/comment',{
+          text: msg,
+          commentId: id,
+        }, {
+          headers: { Authorization: `Bearer ${userToken}`}
+        }).then (res => {
+          if (res.status === 200) {
+            setStatus({
+              severity: 'success',
+              msg: 'Updated comment',
+            });
+            setComments((prev) => prev.map((c) => (c.id === id ? {...c, text: res.data.data.text} : c)))
+          }
+        })
+        console.log('update');
+      } else if (functionMode === 'delete') {
+        // TODO implement delete logic
+        Axios.delete('/comment', {
+          headers: { Authorization: `Bearer ${userToken}`}, 
+          data: {commentId: id},
+        }).then(res => {
+          if(res.status === 200) {
+            setStatus({
+              severity: 'info',
+              msg: 'Deleted comment successfully',
+            })
+            setComments((prev) => prev.filter((c) => c.id !== id));
+          }
+        });
+        
+        console.log('delete');
+      } else {
+        // TODO setStatus (snackbar) to error
+        setStatus({
+          severity: 'error',
+          msg: 'Error!Function Mode is not recognized',
+        })
+      }
+      setIsConfirm(false);
     }
-  }, [functionMode]);
+    
+  }, [functionMode, msg, id, setComments]);
 
   const changeMode = (mode) => {
     setFunctionMode(mode);
@@ -32,9 +73,10 @@ const CommentCard = ({ comment = { id: -1, msg: '' } }) => {
   return (
     <Card sx={{ p: '1rem', m: '0.5rem', display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
       {!(isConfirm && functionMode == 'update') ? (
-        <Typography sx={{ flex: 1 }}>{comment.msg}</Typography>
+        <Typography sx={{ flex: 1 }}>{comment.text}</Typography>
       ) : (
-        <TextField sx={{ flex: 1 }} value={msg} onChange={(e) => setMsg(e.target.value)} />
+        <TextField sx={{ flex: 1 }} value={msg} onChange={(e) => setMsg(e.target.value)} error={error !== ''}
+        helperText={error}/>
       )}
       {!isConfirm ? (
         <Button onClick={() => changeMode('update')} variant="outlined" color="info">
